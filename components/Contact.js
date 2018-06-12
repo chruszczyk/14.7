5397a84941790bf70c88248314f335f25506de6e
Alicja Chruszczyk <alicja.chruszczyk@gmail.com>
2018-06-12 23:47:38 +0200

    first commit

---
+++ components/Contact.js
@@ -0,0 +1,20 @@
+var Contact = React.createClass({
+	propTypes: {
+		item: React.PropTypes.object.isRequired,
+	},
+
+	render: function() {
+		return (
+			<div className={'contactItem'}>
+				<img className={'contactImage'} src={'https://www.shareicon.net/data/128x128/2015/08/23/89482_contact_512x512.png'}/>
+				<p className={'contactLabel'}>
+					Imię: {this.props.contact.firstName}
+				</p>
+				<p className={'contactLabel'}>
+					Nazwisko: {this.props.contact.lastName}
+				</p>
+				<a href={'mailto:' + this.props.item.email}>
+					{this.props.item.email}
+				</a>
+			</div>
+});

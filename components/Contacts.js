16fcb8cb3787eb55988a8c28838501389a33ccea
Alicja Chruszczyk <alicja.chruszczyk@gmail.com>
2018-06-14 21:58:16 +0200

    fix

--- components/Contacts.js
+++ components/Contacts.js
@@ -7,8 +7,8 @@ var Contacts = React.createClass({
 		var contacts = this.props.items.map(function(contact) {
 				return (
 					<Contact item={contact} key={contact.id}></Contact>
-		});
-
+				),
+				
 		return (
 			<ul className={'contactsList'}> {contacts}</ul>
 		);
